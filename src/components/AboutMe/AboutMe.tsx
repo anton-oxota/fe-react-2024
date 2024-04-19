9be04e2c8d6c230385d817ff35c9f9fafe66440d
Anton Oxota <unearthlylight@gmail.com>
2024-04-19 19:43:54 +0300

    About links fixed

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -12,12 +12,19 @@ function AboutMe() {
                 <div className={styles.content}>
                     <h1 className={styles.title}>About me</h1>
                     <p className={styles.text}>
-                        Hi! My name is [Name] and I&apos;m a Junior Frontend Developer. I am already familiar with main Web Technologies
-                        like React, HTML, CSS, JavaScript and Git version control system. <br /> <br />
+                        Hi! My name is Anton and I&apos;m a Junior Frontend Developer. I am already familiar with main Web Technologies like
+                        React, HTML, CSS, JavaScript and Git version control system. <br /> <br />
                         This page was developed during the course &apos;Intro to React&apos; from Masters Academy in 2024. <br /> <br />
                         This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to create my
-                        own small project for the portfolio. <br /> <br /> You can contact me via [social network name] and check out my
-                        GitHub.
+                        own small project for the portfolio. <br /> <br /> You can contact me via{' '}
+                        <a href="https://www.linkedin.com/in/anton-oxota/" target="_black" className={styles.link}>
+                            LinkedIn
+                        </a>{' '}
+                        and check out my{' '}
+                        <a href="https://github.com/anton-oxota" target="_black" className={styles.link}>
+                            GitHub
+                        </a>{' '}
+                        .
                     </p>
                 </div>
             </div>
