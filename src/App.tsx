4ec958971eb13699730a9b654d7449a988108d5d
Anton Oxota <unearthlylight@gmail.com>
2024-04-25 18:12:21 +0300

    task-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import AboutMe from './components/AboutMe/AboutMe.tsx';
 import Footer from './components/Footer/Footer.tsx';
-import Header from './components/Header/Header.tsx';
+import Header from './components/Header/Header.component.tsx';
 
 // import styles from './App.module.css';
 
